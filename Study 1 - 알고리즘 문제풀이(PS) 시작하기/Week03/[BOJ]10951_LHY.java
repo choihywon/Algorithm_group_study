a6f5c4b01e0f5ef0dca74f7223a056be461b3aef
Hyewon <hooho931@gmail.com>
2023-12-16 15:11:30 +0900

    Revert "변경"

--- Study 1 - 알고리즘 문제풀이(PS) 시작하기/Week03/[BOJ]10951_LHY.java
+++ Study 1 - 알고리즘 문제풀이(PS) 시작하기/Week03/[BOJ]10951_LHY.java
@@ -2,25 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		StringTokenizer st;	
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
 		String str;
-		String result = ""; // br
-
-		while((str = br.readLine()) != null) {
-		    st = new StringTokenizer(str, " ");
-
-		    int A = Integer.parseInt(st.nextToken());
-		    int B = Integer.parseInt(st.nextToken());
-
-		    result += (A + B + "\n");
+		
+		while((str=br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+		
+			  int A = Integer.parseInt(st.nextToken());
+		        int B = Integer.parseInt(st.nextToken());
+		         
+		       sb.append(A+B).append("\n");
 		}
-
-		bw.write(result);
-
+	
+       
+		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
